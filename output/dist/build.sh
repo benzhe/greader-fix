dd56159a5d519a9acce45a573c61aa7403792e7a
Ben Zhe <benzhe@users.noreply.github.com>
2025-06-26 09:05:48 +0000

    init

---
+++ output/dist/build.sh
@@ -0,0 +1,8 @@
+apktool b ../
+
+rm aligned.apk
+
+/workspaces/android_sdk/build-tools/35.0.1/zipalign -v -p 4 com.noinnion.android.greader.reader_5.2.2.apk aligned.apk
+/workspaces/android_sdk/build-tools/35.0.1/apksigner sign --ks release-key.jks --ks-pass pass:123456 --out signed.apk aligned.apk
+
+adb install signed.apk
